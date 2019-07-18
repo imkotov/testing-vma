d9593cd42248d7e0e139d6b24631a394196362bb
imkotov <nekk1ch@yandex.ru>
2019-07-18 15:02:18 +0500

    Hash fix

--- bundle.js
+++ bundle.js
@@ -6,8 +6,8 @@
   connect.subscribe((e) => {
     if(e.detail.type == "VKWebAppGetUserInfoResult") {
       $('.username').html(e.detail.data.first_name+' '+e.detail.data.last_name);
-      let id = 'cakeID' + e.detail.data.first_name + '#';
-      $('.hash').html(btoa(id));
+      // $('.hash').html(btoa("cakeID"+e.detail.data.first_name+"#"));
+      $('.hash').html(CryptoJS.MD5("cakeID"+e.detail.data.first_name+"#"));
     }
   });
 
